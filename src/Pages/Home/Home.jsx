a57b4ed252090f0d794d213657b1a500ad4902b1
umarovali <umarovali.345@gmail.com>
2024-04-02 17:40:49 +0600

    footer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Simpley } from "./Simpley/Simpley";
+import Footer from "./Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Simpley />
+      <Footer />
     </>
   );
 }
